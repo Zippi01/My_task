702f438a77a35f41704666d4f8c94fdb34ee727c
Zippi01 <katyakovtun080619@gmail.com>
2020-04-09 18:13:25 +0300

    init

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_04_081036) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "descripion"
